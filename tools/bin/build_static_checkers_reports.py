cb0eb3fc988f5405d195293d54fb3cda23175cfa
Vadym Hevlich <vege1wgw@gmail.com>
2021-12-24 13:21:24 +0200

    Update build reports script

--- tools/bin/build_static_checkers_reports.py
+++ tools/bin/build_static_checkers_reports.py
@@ -2,6 +2,28 @@
 # Copyright (c) 2021 Airbyte, Inc., all rights reserved.
 #
 import sys
+from invoke import Context
+
+from tasks import _run_task
+
+
+CHECKERS = [
+    "black",
+    "coverage",
+    "flake",
+    "isort",
+    "mypy",
+    "test",
+]
+
+
+def build_static_checkers_reports(modules):
+    ctx = Context()
+    for module_path in modules:
+        for checker in CHECKERS:
+            _run_task(ctx, module_path, checker, multi_envs=False)
+
 
 if __name__ == "__main__":
-    print("Changed modules: ", sys.argv)
+    print("Changed modules: ", sys.argv[1:])
+    build_static_checkers_reports(sys.argv[1:])
